39c9113d01aeb8a83d3dfa8ccba0c0c0887fee77
kevin marinho <kevindmarinho4321@gmail.com>
2022-10-11 14:03:10 -0400

    Initial Commit

---
+++ ScanHalloween/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScanHalloween
+//
+//  Created by kevin marinho on 11/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScanHalloween/ScanHalloweenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanHalloweenApp.swift
+//  ScanHalloween
+//
+//  Created by kevin marinho on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ScanHalloweenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
