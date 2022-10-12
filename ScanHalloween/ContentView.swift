ccc889b6a1a0ce050c992a0bf29f144ca5575efe
kevin marinho <kevindmarinho4321@gmail.com>
2022-10-12 00:51:49 -0400

    add coisas

--- ScanHalloween/ContentView.swift
+++ ScanHalloween/ContentView.swift
@@ -6,16 +6,40 @@
 //
 
 import SwiftUI
+import CodeScanner
 
-struct ContentView: View {
+struct ScanHalloween: View {
+    @State var isPresentingScanner = false
+    @State var scannedCode: String = "Leia para tentar desvender o misterio"
+    
+    var scannerSheet : some View {
+        CodeScannerView(
+            codeTypes: [.qr],
+            completion: { result in
+                if case let .success(code) = result{
+                    self.scannedCode = code.string
+                    self.isPresentingScanner = false
+                }
+            }
+        )
+    }
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text(scannedCode)
+            
+            Button("Leia essa parada ai"){
+                self.isPresentingScanner = true
+            }
+            .sheet(isPresented: $isPresentingScanner){
+                self.scannerSheet
+            }
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ScanHalloween()
     }
 }
--- ScanHalloween/ghostView.swift
+++ ScanHalloween/ghostView.swift
@@ -6,10 +6,27 @@
 //
 
 import SwiftUI
+import ARKit
+
+
+struct ARView: UIViewRepresentable{
+    func makeUIView(context: Context) -> some UIView {
+        let sceneView = ARSCNView()
+        sceneView.showsStatistics = true
+        
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+        return sceneView
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+}
 
 struct ghostView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ARView()
     }
 }
 
