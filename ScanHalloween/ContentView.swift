5f5347c0f5e9c6e5acd08b5470077f9754e45cad
Leticia <leticiavictoria99@gmail.com>
2022-10-12 02:13:33 -0400

    view

--- ScanHalloween/ContentView.swift
+++ ScanHalloween/ContentView.swift
@@ -11,7 +11,7 @@ import CodeScanner
 struct ScanHalloween: View {
     @State var isPresentingScanner = false
     @State var isPresentingModal = false
-    @State var scannedCode: String = "Leia para tentar desvender o misterio"
+    @State var scannedCode: String = "Escaneie os QR codes espalhados pelo lab e desvende os enigmas propostos. Lembre-se de explorar todo o ambiente e tenha uma experiência horripilante."
     
     var scannerSheet : some View {
         CodeScannerView(
@@ -26,11 +26,52 @@ struct ScanHalloween: View {
         )
     }
     var body: some View {
-        VStack{
-            Text(scannedCode)
+        
+        ZStack{
             
-            Button("Leia essa parada ai"){
-                self.isPresentingScanner = true
+            Image("backgroundInicio")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .ignoresSafeArea()
+
+          
+            VStack{
+                ZStack{
+                    Image("teia")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Spacer()
+                    Image("title")
+                        .padding(.top, 94)
+                    Spacer()
+                }
+                .padding(.bottom, 36.48)
+               
+                Text("Escaneie os QR codes espalhados pelo lab e desvende os enigmas propostos. Lembre-se de explorar todo o ambiente e tenha uma experiência horripilante.")
+                    .font(.system(size: 18))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 26)
+                    .padding(.bottom, 23)
+                
+                Image("ghost")
+                    .resizable()
+                    .frame(width: 220.5, height: 252)
+                    .padding(.bottom, 62)
+                
+                Button{
+                    self.isPresentingScanner = true
+                }label:{
+                    Text("Escanear QR Code")
+                        .bold()
+                        .padding(.vertical, 25)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.orange)
+                        .foregroundColor(.white)
+                        .cornerRadius(40)
+                        .padding(.horizontal, 25)
+                }
+                
+                Spacer()
             }
             .sheet(isPresented: $isPresentingScanner){
                 self.scannerSheet
@@ -38,7 +79,6 @@ struct ScanHalloween: View {
             .sheet(isPresented: $isPresentingModal){
                 self.modal
             }
-            
         }
     }
     
@@ -48,6 +88,11 @@ struct ScanHalloween: View {
             Text(scannedCode)
         }
     }
+}
+
+extension Color{
+    
+    static let background = Color("ColorBackground")
 }
 
 
