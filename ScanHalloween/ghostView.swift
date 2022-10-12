dcdea2d9504ec4e1b9fe4b3fa9d5bdeabff16db3
kevin marinho <kevindmarinho4321@gmail.com>
2022-10-12 00:24:07 -0400

    first commit

---
+++ ScanHalloween/ghostView.swift
@@ -0,0 +1,20 @@
+//
+//  ghostView.swift
+//   Hallowedemy
+//
+//  Created by kevin marinho on 11/10/22.
+//
+
+import SwiftUI
+
+struct ghostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ghostView_Previews: PreviewProvider {
+    static var previews: some View {
+        ghostView()
+    }
+}
